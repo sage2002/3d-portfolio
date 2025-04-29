1a801daeee6bc99bc01956c2496d9346cd154a61
sage2002 <gubai17092002@gmail.com>
2025-04-29 19:42:17 +0530

    Fixed glowcard

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -26,12 +26,15 @@ const GlowCard = ({ card, children, index}) => {
     <div ref={(el) => (cardRefs.current[index] = el)} 
     onMouseMove={ handleMouseMove (index)} className="card card-border timeline-card rounded-xl p-10 mb-5 break-inside-avoid-column">
         <div className="glow"/>
-        <div className="flex items-center gap-1 mb-5">
+        {/* <div className="flex items-center gap-1 mb-5">
             {Array.from({length: 5}, (_, i) =>(
                 <img src="/images/star.png" key={i} alt="star" className="size-5" />
             ))}
-        </div>
-    <div className="mb-5">
+        </div> */}
+        <p className='flex items-center gap-1 mb-5 break-inside-avoid-column text-2xl font-bold text-white'>
+          {card.courseDuration}
+        </p>
+        <div className="mb-5">
         <p className="text-white-50 text-lg">{card.review}</p>
         </div>
         {children}
