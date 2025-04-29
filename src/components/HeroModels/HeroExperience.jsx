8fecb2c98ed3c896f7cb10870d93b9366da6cbd2
sage2002 <gubai17092002@gmail.com>
2025-04-29 19:42:34 +0530

    Fixed update

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -22,7 +22,7 @@ const HeroExperience = () => {
 
         <HeroLights />
 
-        <Particles count={50} />
+        <Particles count={90} />
         <group
             scale={isMobile? 0.7 : 1}
             position={[0, -3.5, 0]}
