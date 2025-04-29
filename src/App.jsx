9d552a38982e30594fab7b5cfb77aba8868c94f0
sage2002 <gubai17092002@gmail.com>
2025-04-30 00:05:05 +0530

    App updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import ExperienceSection from "./sections/QualificationSection"
 import TechStack from "./sections/TechStack"
 import Testimonials from "./sections/Testimonials"
 import Contact from "./sections/Contact"
+import Footer from "./sections/Footer"
 const App = () => {
   return (
     <>
@@ -19,6 +20,7 @@ const App = () => {
       <TechStack />
       <Testimonials />
       <Contact />
+      <Footer />
       
     </>
   )
